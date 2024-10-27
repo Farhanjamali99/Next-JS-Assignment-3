856c3f288c90e89645ea5405a839618e6fd0f85d
Farhanjamali99 <farhanjamali2222@gmail.com>
2024-10-27 22:49:00 +0500

    Link

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 const Header = () => {
     return(
         <header className="bg-gray-800 text-white p-4">
     <nav className="container mx-auto flex justify-between items-center">
-      <a href="/"><h1 className="hover:text:gray-300 text-xl font-bold ">Web-Developer</h1></a>
+      <Link href="/"><h1 className="hover:text:gray-300 text-xl font-bold ">Web-Developer</h1></Link>
       <ul className="flex space-x-4">
       <li><a href="/" className="hover:text-blue-300">Home</a></li>
         <li><a href="/About" className="hover:text-blue-300">About</a></li>
